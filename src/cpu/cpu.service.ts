768ccb31b16b1a57d1d509788af8e74f179f9a50
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2024-01-04 13:19:49 +0530

    connect CPU and DISK to Power module

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -3,7 +3,10 @@ import { PowerService } from 'src/power/power.service';
 
 @Injectable()
 export class CpuService {
-  constructor(private powerService: PowerService){
-
+  constructor(private powerService: PowerService){}
+  compute(a: number, b: number){
+    console.log('Drawing 10 watts of power from Power Service');
+    this.powerService.supplyPower(10);
+    return a + b;
   }
 }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerservice: PowerService){}
+
+  getData(){
+    console.log('Drawing 20 watts of power from powerServices');
+    this.powerservice.supplyPower(20);
+    return 'data!';
+  }
+}
